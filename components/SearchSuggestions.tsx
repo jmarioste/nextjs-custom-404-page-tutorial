052a2afb2c33dca133e77337164ead876f919c91
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-02-24 16:59:09 +0800

    refactoring + add comments

--- components/SearchSuggestions.tsx
+++ components/SearchSuggestions.tsx
@@ -1,46 +1,54 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useFetch } from "usehooks-ts";
-type Product = {
-  title: string;
-};
 
+// Step 0 : Define the types
+// API Response
 type SearchResponse = {
-  products: Product[];
+  products: {
+    title: string;
+  }[];
 };
 
 type SearchSuggestion = {
   title: string;
   url: string;
 };
+
 const SearchSuggestions = () => {
   const router = useRouter();
+  // Step 1: Parse the router path
+
   const query =
     router.asPath
       .split("/")
       .filter((q) => !!q)
       .pop() ?? ""; // transforms: "/products/iphone" into "iphone"
 
-  // create the URL
+  // Step 2: Create the URL
+  // We're using dummyjson as an example
+  // Most likely you have your own search api or If you're using algolia, that's even better.
   const url = new URL("https://dummyjson.com/products/search");
   url.searchParams.set("q", query);
   url.searchParams.set("limit", "4");
 
-  // Call the search API using useFetch. This can be improved If you have your search API
+  // Step 3: Call the search API using useFetch. This can be improved If you have your search API
   const { data = { products: [] }, error } = useFetch<SearchResponse>(
     url.toString()
   );
 
-  // list of suggestions to show the user
+  // Step 4: Map the results to our list of suggestions to show the user
   const suggestions = data?.products.map<SearchSuggestion>((p) => {
     return {
       title: p.title,
+      // using home page route since the api doesn't return some kind of URL.
       url: "/",
     };
   });
 
   if (!data || error) return null;
 
+  // Step 5: render the alternative suggestions
   return (
     <ul className="grid grid-cols-2 my-2">
       {suggestions.map((item, index) => (
@@ -56,4 +64,5 @@ const SearchSuggestions = () => {
     </ul>
   );
 };
+
 export default SearchSuggestions;
